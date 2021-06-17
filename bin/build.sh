5368ac618787224eac3ee972662b1ac910b58193
Ramanaiah-dot-Grande <85276690+Ramanaiah-dot-Grande@users.noreply.github.com>
2021-06-18 01:28:43 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ source $1
 sed -i "s/App version/App ${VERSION}/g" ${APP_HOME}/src/main.py
 
 # Build Docker file
-docker build -t ramananew/hello-python1:${VERSION} ${APP_HOME}/.
+docker build -t ramananew/hello-python:${VERSION} ${APP_HOME}/.
 
 # Push image to docker hub
-docker push ramananew/hello-python1:${VERSION}
+docker push ramananew/hello-python:${VERSION}
